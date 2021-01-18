0d4b4e7e95b3634036fb9674a2ca8c468bce0425
yumingjie <yumingjie@kingsoft.com>
2021-01-18 10:26:57 +0800

    change-cron-config

--- cron_install.sh
+++ cron_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat > /var/spool/cron/crontabs/root << EOF
-# */10 * * * * sh /data/work/tyymj.github.io/update_ip.sh > /data/work/tyymj.github.io/change.log 2>&1
+*/10 * * * * sh /data/work/tyymj.github.io/update_ip.sh > /data/work/tyymj.github.io/change.log 2>&1
 EOF
 service cron restart
