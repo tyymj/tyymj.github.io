9975387de4635ca649cddb00614adebc2dd7de82
tyymj <tyymj@126.com>
2021-02-22 22:39:09 +0800

    change-cron

--- cron_install.sh
+++ cron_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cat > /var/spool/cron/crontabs/root << EOF
+service cron stop
+sudo cat > /var/spool/cron/crontabs/pi << EOF
 */10 * * * * sh /data/work/tyymj.github.io/update_ip.sh > /data/work/tyymj.github.io/change.log 2>&1
 EOF
 service cron restart
