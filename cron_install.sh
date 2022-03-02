6977816755578e98ab8ccdab01d68887cbac0c63
canger <tyymj@126.com>
2022-03-02 10:39:35 +0800

    fix-path-2

--- cron_install.sh
+++ cron_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 service cron stop
-sudo cat > /var/spool/cron/crontabs/pi << EOF
-*/10 * * * * sh /data/work/tyymj.github.io/update_ip.sh > /data/work/tyymj.github.io/change.log 2>&1
+cat > /var/spool/cron/crontabs/pi << EOF
+*/2 * * * * sh /data/projects/tyymj.github.io/update_ip.sh > /data/projects/tyymj.github.io/change.log 2>&1
 EOF
 service cron restart
